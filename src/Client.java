7b22f828eae73443aec815001308309869d39ecd
Niels Richters <nrichters@gmx.ch>
2022-12-02 10:34:51 +0100

    small client improvements

--- src/Client.java
+++ src/Client.java
@@ -3,115 +3,91 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client extends Thread {
-    private Socket socket;
-    private PrintWriter writer;
-    private BufferedReader reader;
+    private final Socket socket;
+    private final PrintWriter writer;
+    private final BufferedReader reader;
 
     public Client(String ip, int port) throws IOException {
-        setSocket(new Socket(ip, port));
-        setWriter(new PrintWriter(getSocket().getOutputStream()));
-        setReader(new BufferedReader(new InputStreamReader(getSocket().getInputStream())));
-        getReaderThread().start();
-        getWriterThread().start();
+        socket = new Socket(ip, port);
+        writer = new PrintWriter(socket.getOutputStream());
+        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        //init reader
+        Thread readerThread = new Thread(() -> {
+            try {
+                reader();
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        readerThread.start();
+
+        //init writer
+        Thread writerThread = new Thread(() -> {
+            try {
+                writer();
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        writerThread.start();
     }
-
-    private final Thread readerThread = new Thread(() -> {
-        try {
-            reader();
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    });
-
-    private final Thread writerThread = new Thread(() -> {
-        try {
-            writer();
-        } catch (IOException | InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    });
 
     private void writer() throws IOException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
+        boolean isLoggedIn = false;
+
         while (true) {
+            if (!isLoggedIn)
+                System.out.printf("username: ");
+
             String message = scanner.nextLine();
-            switch (message) {
-                case "HELP" -> help();
-                default -> {
-                    getWriter().println(message);
-                    getWriter().flush();
-                }
+
+            if ("HELP".equals(message)) {
+                help();
+            } else if (!isLoggedIn) {
+                isLoggedIn = true;
+
+                writer.println("IDENT " + message);
+                writer.flush();
+            } else {
+                writer.println("BCST " + message);
+                writer.flush();
             }
         }
     }
 
     private void reader() throws IOException, InterruptedException {
         while (true) {
-            String message = getReader().readLine();
+            String message = reader.readLine();
             switch (message) {
-                case "PING":
-                    pong();
-                    break;
-                case "OK Goodbye":
+                case "PING" -> {
+                    writer.println("PONG");
+                    writer.flush();
+                }
+                case "OK Goodbye" -> {
                     System.out.println(message);
-                    getReader().close();
-                    getWriter().close();
-                    getSocket().close();
+                    reader.close();
+                    writer.close();
+                    socket.close();
                     System.exit(0);
-                default:
-                    System.out.println(message);
-                    break;
+                }
+                default -> System.out.println(message);
             }
         }
     }
 
-    public void pong() {
-        getWriter().println("PONG");
-        getWriter().flush();
-    }
-
     private void help() {
-        System.out.println(
+        Helper.printLineColour(
                 """
-                Welcome to the Java chat server!
-                -------
-                IDENT <username>
-                for logging in.
-                QUIT
-                logs current user out.
-                BCST <message>
-                broadcasts message to all users that are logged in.""");
-    }
-
-    public PrintWriter getWriter() {
-        return writer;
-    }
-
-    public void setWriter(PrintWriter writer) {
-        this.writer = writer;
-    }
-
-    public Socket getSocket() {
-        return socket;
-    }
-
-    public void setSocket(Socket socket) {
-        this.socket = socket;
-    }
-
-    public BufferedReader getReader() {
-        return reader;
-    }
-
-    public void setReader(BufferedReader reader) {
-        this.reader = reader;
-    }
-
-    public Thread getReaderThread() {
-        return readerThread;
-    }
-
-    public Thread getWriterThread() {
-        return writerThread;
+                        Welcome to the Java chat server!
+                        -------
+                        IDENT <username>
+                        for logging in.
+                        QUIT
+                        logs current user out.
+                        BCST <message>
+                        broadcasts message to all users that are logged in.""",
+                Helper.ConsoleColour.YELLOW);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        new Client("127.0.0.1", 1337);
+        new Client("13.94.156.82", 1337);
+//        new Client("127.0.0.1", 1337);
     }
 }
