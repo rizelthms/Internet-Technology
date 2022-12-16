faca0b175e6dee3513e5e69177c9deeea66b57a2
Rizelthomas <497836@student.saxion.nl>
2022-12-16 16:56:43 +0100

    Added a functionality to the client side as well

--- Client/src/Client/Client.java
+++ Client/src/Client/Client.java
@@ -21,16 +21,20 @@ public class Client extends Thread {
 
     public Client(String ip, int port) throws IOException {
         try {
+            // Create socket and connect to server
             socket = new Socket(ip, port);
+            // Initialize writer for sending messages to server
             writer = new PrintWriter(socket.getOutputStream());
+            // Initialize reader for reading messages from server
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
             //init reader
             new Thread(() -> {
                 try {
                     reader();
-                } catch (IOException | InterruptedException e) {
+                } catch (Exception e) {
                     Printer.printLineColour("Problems connecting to the server!", Printer.ConsoleColour.RED);
+                    Printer.printLineColour(e.getMessage(), Printer.ConsoleColour.RED);
                 }
             }).start();
 
@@ -59,11 +63,17 @@ public class Client extends Thread {
             if (!message.isEmpty()) {
                 if (message.startsWith(Protocol.QUIT)) {
                     writer.println(message);
-                } else if (!isLoggedIn) {
+                }else if (!isLoggedIn) {
                     isLoggedIn = true;
 
                     writer.println(Protocol.IDENTIFY + " " + message);
-                } else {
+                }else if(message.startsWith(Protocol.USERS)){
+                    Printer.printLineColourBold("List of connected users:", Printer.ConsoleColour.YELLOW);
+                    writer.println(message);
+                }else if(message.startsWith(Protocol.PRIVATE_MSG)){
+                    writer.println(message);
+                }
+                else{
                     writer.println(Protocol.BROADCAST + " " + message);
                 }
                 writer.flush();
@@ -71,9 +81,11 @@ public class Client extends Thread {
         }
     }
 
-    private void reader() throws IOException, InterruptedException {
+    private void reader() throws Exception{
         while (true) {
-            var message = reader.readLine().split(" ");
+            String messageFull = reader.readLine();
+            if(messageFull==null){continue;}
+            var message = messageFull.split(" ");
 
             switch (message[0]) {
                 case Protocol.OK -> {
@@ -89,7 +101,18 @@ public class Client extends Thread {
                         Printer.printLineColourBold(
                                 "Welcome " + message[2] + "!",
                                 Printer.ConsoleColour.GREEN);
+                    } else if(message[1].equals(Protocol.USERS)){
+                        //Display server response to request for user list
+                        Printer.printLineColourBold(message[2], Printer.ConsoleColour.YELLOW);
                     }
+                }
+                case Protocol.DISCONNECT -> {
+                    System.out.println("Disconnected by server!");
+
+                    reader.close();
+                    writer.close();
+                    socket.close();
+                    System.exit(0);
                 }
                 case Protocol.PING -> {
                     writer.println(Protocol.PONG);
@@ -110,13 +133,21 @@ public class Client extends Thread {
                             "[" + message[1] + "] " + String.join(" ", Arrays.copyOfRange(message, 2, message.length)),
                             users.stream().filter(user -> user.username().equals(message[1])).findFirst().get().colour());
                 }
+                case Protocol.RECEIVE_PRIVATE_MSG -> {
+                    Printer.printLineColour("PRIVATE MESSAGE " + "[" + message[1] + "] " + String.join(" ", Arrays.copyOfRange(message, 2, message.length)), Printer.ConsoleColour.PURPLE);
+                }
+                case Protocol.JOINED -> {
+                    if(isLoggedIn) {
+                        Printer.printLineColourBold(message[1] + " has joined the chat!", Printer.ConsoleColour.GREEN);
+                    }
+                }
                 // all fails
                 default -> {
                     Printer.printLineColour(
                             String.join(" ", Arrays.copyOfRange(message, 1, message.length)),
                             Printer.ConsoleColour.RED);
 
-                    if (message[0].equals(Protocol.FAIL01) || message[0].equals(Protocol.FAIL02)) {
+                    if (message[0].equals(Protocol.FAIL01) || message[0].equals(Protocol.FAIL02) || message[0].equals(Protocol.FAIL03)) {
                         isLoggedIn = false;
                         Printer.printColourBold("username: ", Printer.ConsoleColour.WHITE);
                     }
