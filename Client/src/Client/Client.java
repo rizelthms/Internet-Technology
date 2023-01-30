cd1c23e318d450382f5d2af24ed9afeb5d6baeda
Rizelthomas <497836@student.saxion.nl>
2023-01-30 14:32:45 +0100

    Implemented encryption and Added comments

--- Client/src/Client/Client.java
+++ Client/src/Client/Client.java
@@ -2,22 +2,33 @@ package Client;
 
 import Shared.Printer;
 import Shared.Protocol;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.*;
+import java.security.*;
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+
+import static Client.Encryption.*;
+
 
 public class Client extends Thread {
     private Socket socket;
-    private PrintWriter writer;
+    private static PrintWriter writer;
     private BufferedReader reader;
     private boolean isLoggedIn = false;
+    // List to store connected users
     ArrayList<User> users = new ArrayList<>();
+    // KeyPair to store the RSA keys
+    KeyPair RSAkeys;
+    // HashMap to store public keys of all connected users
+    static HashMap<String, String> publicKeys;
+    // HashMap to store session keys for each private message communication
+    static HashMap<String, String> sessionKeys;
 
     public Client(String ip, int port) throws IOException {
         try {
@@ -27,6 +38,13 @@ public class Client extends Thread {
             writer = new PrintWriter(socket.getOutputStream());
             // Initialize reader for reading messages from server
             reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // Initialize the publicKeys HashMap
+            publicKeys = new HashMap<String, String>();
+            // Initialize the sessionKeys HashMap
+            sessionKeys = new HashMap<String, String>();
+
+            //Generate RSA keys
+            RSAkeys = generateKeyPair();
 
             //init reader
             new Thread(() -> {
@@ -35,6 +53,7 @@ public class Client extends Thread {
                 } catch (Exception e) {
                     Printer.printLineColour("Problems connecting to the server!", Printer.ConsoleColour.RED);
                     Printer.printLineColour(e.getMessage(), Printer.ConsoleColour.RED);
+                    e.printStackTrace();
                 }
             }).start();
 
@@ -56,7 +75,7 @@ public class Client extends Thread {
 
         while (true) {
             if (!isLoggedIn)
-                Printer.printColourBold("username: ", Printer.ConsoleColour.WHITE);
+                Printer.printColourBold("Username: ", Printer.ConsoleColour.WHITE);
 
             String message = scanner.nextLine();
 
@@ -69,11 +88,31 @@ public class Client extends Thread {
                     writer.println(Protocol.IDENTIFY + " " + message);
                 }else if(message.startsWith(Protocol.USERS)){
                     Printer.printLineColourBold("List of connected users:", Printer.ConsoleColour.YELLOW);
+                    // Send the USERS message to the server
                     writer.println(message);
                 }else if(message.startsWith(Protocol.PRIVATE_MSG)){
-                    writer.println(message);
+                    var messageSplit = message.split(" ");
+                    // Call shareSessionKey to generate and share the session key if it is not already stored
+                    shareSessionKey(messageSplit[1]);
+
+                    try {
+                        // Encrypt the message using the session key and send it to the recipient
+                        String msgToEncrypt = String.join(" ", Arrays.copyOfRange(messageSplit, 2, messageSplit.length));
+                        // Retrieve the session key from the map of session keys
+                        String strSessionKey = sessionKeys.get(messageSplit[1]);
+                        SecretKey sessionKey = new SecretKeySpec(Base64.getDecoder().decode(strSessionKey), "AES");
+                        // Encrypt the message using the AES algorithm
+                        String encryptedMessage = encryptAES(msgToEncrypt, sessionKey);
+                        // Send the encrypted message to the recipient
+                        writer.println(Protocol.PRIVATE_MSG + " " + messageSplit[1] + " " + encryptedMessage);
+                    }
+                    catch(Exception e){
+                        System.out.println("Error sending private message: " + e.getMessage());
+                        e.printStackTrace();
+                    }
                 }
                 else{
+                    // If the message is not a special command, send it as a broadcast message
                     writer.println(Protocol.BROADCAST + " " + message);
                 }
                 writer.flush();
@@ -98,9 +137,12 @@ public class Client extends Thread {
                         System.exit(0);
 
                     } else if (message[1].equals(Protocol.IDENTIFY)) {
-                        Printer.printLineColourBold(
-                                "Welcome " + message[2] + "!",
-                                Printer.ConsoleColour.GREEN);
+                        Printer.printLineColourBold("Welcome " + message[2] + "!", Printer.ConsoleColour.GREEN);
+
+                        //Share public key
+                        writer.println(Protocol.SEND_PUBLIC_KEY + " " + Base64.getEncoder().encodeToString(RSAkeys.getPublic().getEncoded()));
+                        writer.flush();
+
                     } else if(message[1].equals(Protocol.USERS)){
                         //Display server response to request for user list
                         Printer.printLineColourBold(message[2], Printer.ConsoleColour.YELLOW);
@@ -123,7 +165,7 @@ public class Client extends Thread {
                         Printer.ConsoleColour.GREEN);
                 case Protocol.BROADCAST -> {
                     if (users.stream().noneMatch(user -> user.username().equals(message[1])))
-                        // select 1 of 4 colours (the last four are for server messages)
+                        // Select 1 of 4 colours (the last four are for server messages)
                         users.add(
                                 new User(
                                         message[1],
@@ -134,12 +176,44 @@ public class Client extends Thread {
                             users.stream().filter(user -> user.username().equals(message[1])).findFirst().get().colour());
                 }
                 case Protocol.RECEIVE_PRIVATE_MSG -> {
-                    Printer.printLineColour("PRIVATE MESSAGE " + "[" + message[1] + "] " + String.join(" ", Arrays.copyOfRange(message, 2, message.length)), Printer.ConsoleColour.PURPLE);
+                    //Retrieve session key
+                    SecretKey sessionKey = new SecretKeySpec(Base64.getDecoder().decode(sessionKeys.get(message[1])), "AES");
+
+                    //Decrypt message
+                    String decryptedMessage = decryptAES(message[2], sessionKey);
+
+                    Printer.printLineColour("PRIVATE MESSAGE " + "[" + message[1] + "] " + decryptedMessage, Printer.ConsoleColour.PURPLE);
+                    //Printer.printLineColour("PRIVATE MESSAGE " + "[" + message[1] + "] " + String.join(" ", Arrays.copyOfRange(message, 2, message.length)), Printer.ConsoleColour.PURPLE);
                 }
                 case Protocol.JOINED -> {
                     if(isLoggedIn) {
                         Printer.printLineColourBold(message[1] + " has joined the chat!", Printer.ConsoleColour.GREEN);
+
+                        //Share public key
+                        writer.println(Protocol.SEND_PUBLIC_KEY + " " + Base64.getEncoder().encodeToString(RSAkeys.getPublic().getEncoded()));
+                        writer.flush();
                     }
+                }
+                case Protocol.LEFT -> {
+                    if(isLoggedIn) {
+                        Printer.printLineColourBold(message[1] + " has left the chat!", Printer.ConsoleColour.YELLOW);
+
+                        //Delete session key
+                        if(sessionKeys.containsKey(message[1])){
+                            sessionKeys.remove(message[1]);
+                        }
+                    }
+                }
+                case Protocol.SEND_PUBLIC_KEY -> {
+                    //Store the new users public key
+                    publicKeys.put(message[1], message[2]);
+                    writer.println(Protocol.OK + " " + Protocol.SEND_PUBLIC_KEY);
+                }
+                case Protocol.SEND_SESSION_KEY -> {
+                    //Decrypt session key
+                    String sessionKey = decryptRSA(message[2], RSAkeys.getPrivate());
+                    sessionKeys.put(message[1], sessionKey);
+                    writer.println(Protocol.OK + " " + Protocol.SEND_SESSION_KEY);
                 }
                 // all fails
                 default -> {
@@ -149,10 +223,37 @@ public class Client extends Thread {
 
                     if (message[0].equals(Protocol.FAIL01) || message[0].equals(Protocol.FAIL02) || message[0].equals(Protocol.FAIL03)) {
                         isLoggedIn = false;
-                        Printer.printColourBold("Username: ", Printer.ConsoleColour.WHITE);
+                        Printer.printColourBold("username: ", Printer.ConsoleColour.WHITE);
                     }
                 }
             }
+        }
+    }
+
+    private static void shareSessionKey(String username){
+        try {
+            // Check if the session key already exists for the username
+            if(!sessionKeys.containsKey(username)){
+                // Generate a new AES session key
+                KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+                keyGenerator.init(128);
+                SecretKey sessionKey = keyGenerator.generateKey();
+                // Store the encoded session key in the sessionKeys map
+                sessionKeys.put(username, Base64.getEncoder().encodeToString(sessionKey.getEncoded()));
+
+                //Encrypt with RSA
+                //Change string public key back to public key object
+                KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+                PublicKey pubKey = (PublicKey) keyFactory.generatePublic(new X509EncodedKeySpec(Base64.getDecoder().decode(publicKeys.get(username))));
+                //Encrypt the session key with the public key
+                String encryptedSessionKey = Encryption.encryptRSA(Base64.getEncoder().encodeToString(sessionKey.getEncoded()), pubKey);
+                //Send the encrypted session key to the recipient
+                writer.println(Protocol.SEND_SESSION_KEY + " " + username + " " + encryptedSessionKey);
+            }
+        }
+        catch(Exception e){
+            System.out.println("Error generating session key: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
--- Server/src/ServerThread.java
+++ Server/src/ServerThread.java
@@ -1,9 +1,6 @@
 import Shared.Printer;
 import Shared.Protocol;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.util.Arrays;
 import java.util.regex.Matcher;
@@ -36,6 +33,7 @@ public class ServerThread extends Thread {
             pingPongFlag = false;
             //Send welcome message and read message from client
             writer.println(Protocol.INITIALISE + " Welcome to the server!");
+            writer.flush();
 
             //Start a new thread to handle the ping-pong(heartbeat) mechanism for the client
             pingPongThread = new Thread(new pingPong(this));
@@ -47,6 +45,7 @@ public class ServerThread extends Thread {
             this.stop();
         }
     }
+
     public void run(){
         try {
             // Listen for incoming messages in an infinite loop
@@ -67,7 +66,7 @@ public class ServerThread extends Thread {
                         if(username == null) {
                             createUser(msg);
                         }else{
-                            //Already logged in
+                            //already logged in
                             writer.println(Protocol.FAIL04);
                         }
                     }
@@ -81,10 +80,11 @@ public class ServerThread extends Thread {
                     case Protocol.QUIT -> {
                         // Client is trying to disconnect
                         writer.println(Protocol.OK + " " + Protocol.GOODBYE);
+                        broadcastMessage(Protocol.LEFT + " " + username);
                         server.users.remove(this);
                         writer.close();
                         reader.close();
-                        socket.close();
+                        //socket.close();
                         pingPongThread.stop();
                         this.stop();
                     }
@@ -96,40 +96,66 @@ public class ServerThread extends Thread {
                         // Client is trying to send a private message
                         privateMsg(String.join(" ", Arrays.copyOfRange(msg, 2, msg.length)) ,msg[1]);
                     }
+                    case Protocol.SEND_PUBLIC_KEY -> {
+                        // Broadcast the public key to all users
+                        broadcastMessage(messageFull);
+                    }
+                    case Protocol.SEND_SESSION_KEY -> {
+                        // Share session key with other client
+                        shareSessionKey(messageFull);
+                    }
+                    case Protocol.OK -> {
+
+                    }
                     default -> {
                         //Unknown command
                         writer.println(Protocol.FAIL00);
                     }
                 }
+                writer.flush();
             }
         }catch (Exception e){
+            // Print error message and remove user from server's user list
             Printer.printLineColour(e.getMessage(), Printer.ConsoleColour.RED);
             server.users.remove(this);
             pingPongThread.stop();
             this.stop();
         }
     }
+
+    //Broadcast to all clients except the sender of the message
     private void broadcastMessage(String msg){
         for(int i=0; i < server.users.size(); i++){
             //Broadcast to all clients except the sender of the message
             if(msg.startsWith(Protocol.BROADCAST) && server.users.get(i)!=this){
-                //Reformat string, insert the username of the sender
+                // Reformat the string, insert the username of the sender into the message
                 server.users.get(i).writer.println(new StringBuilder(msg).insert(Protocol.BROADCAST.length(), " " + this.username).toString());
+                // Check if the message starts with Protocol.JOINED and the current client is not the sender of the message
             }else if(msg.startsWith(Protocol.JOINED) && server.users.get(i)!=this) {
                 server.users.get(i).writer.println(msg);
             }else if(msg.startsWith(Protocol.BROADCAST) && server.users.get(i)==this){
-                //Confirmation response sent to the client who sent this message
+                // Confirmation response sent to the client who sent this message
                 server.users.get(i).writer.println(Protocol.OK + " " + msg);
+            }else if(msg.startsWith(Protocol.LEFT) && server.users.get(i)!=this){
+                server.users.get(i).writer.println(msg);
+            }else if(msg.startsWith(Protocol.SEND_PUBLIC_KEY) && server.users.get(i)!=this){
+                //Reformat string, insert the username of the sender
+                String[] msgArr = msg.split(" ");
+                server.users.get(i).writer.println(Protocol.SEND_PUBLIC_KEY + " " + this.username + " " + msgArr[1]);
             }
         }
     }
 
     //Sends a list of current users to the client that requested it
     private void userList(){
+        // Initialize an empty string to store the list of users
         String userList = "";
+        // Counter to keep track of the number of logged-in users
         int loggedInUsers = 0;
         for(int i=0; i < server.users.size(); i++) {
+            // Check if the user has a username (i.e. is logged in)
             if(server.users.get(i).username!=null) {
+                // Add the user's username to the list of users
                 userList=userList.concat(server.users.get(i).username + ":");
                 loggedInUsers++;
             }
@@ -137,38 +163,53 @@ public class ServerThread extends Thread {
         if(loggedInUsers==1){
             writer.println(Protocol.FAIL06);
         }else if(loggedInUsers>0){
-            writer.println(Protocol.OK + " " + Protocol.USERS + " " + userList.substring(0, userList.length() - 1)); //substring removes last unnecessary : from the string
+            // Send the list of users to the client
+            writer.println(Protocol.OK + " " + Protocol.USERS + " " + userList.substring(0, userList.length() - 1));
+            // Remove the last unnecessary colon (:) from the list of users
             System.out.println(Protocol.OK + " " + Protocol.USERS + " " + userList.substring(0, userList.length() - 1));
         }
     }
 
     //Sends a private message to the specified user
     private void privateMsg(String msg, String user){
+        //Flag to track if the target user exists
         boolean userExists = false;
+        //Iterate over the list of all connected clients
         for(int i=0; i < server.users.size(); i++) {
             if(server.users.get(i).username!=null && server.users.get(i).username.equals(user)) {
                 writer.println(Protocol.OK + " " + Protocol.PRIVATE_MSG + " " + user + " " + msg); //Confirm/approve the command
+                writer.flush();
                 server.users.get(i).writer.println(Protocol.RECEIVE_PRIVATE_MSG + " " + username + " " + msg); //Send the message to the destined receiver
                 userExists=true;
                 break;
             }
         }
         if(!userExists){
+            //If userExists is false, target user not found
             writer.println(Protocol.FAIL07);
+        }
+    }
+
+    private void shareSessionKey(String msg){
+        String[] msgArr = msg.split(" ");
+
+        for(int i=0; i < server.users.size(); i++) {
+            if (server.users.get(i).username != null && server.users.get(i).username.equals(msgArr[1])) {
+                server.users.get(i).writer.println(Protocol.SEND_SESSION_KEY + " " + this.username + " " + msgArr[2]);
+                break;
+            }
         }
     }
 
     //Sets the correct username for the user, i.e. creates a new user
     private void createUser(String[] msgArr){
-        //Check for errors
-
-        //User already logged in
+        // Check if the user is already logged in
         for(int i=0; i < server.users.size(); i++){
-            if(server.users.get(i)!=this){
+            if(server.users.get(i)!=this){// check current user with all other users
                 if(server.users.get(i).username!=null && server.users.get(i).username.equals(msgArr[1])){
+                    // If the user is already logged in, send failure message
                     writer.println(Protocol.FAIL01);
                     return;
-                    //continue outer; //continue the outer (while) loop
                 }
             }
         }
@@ -177,18 +218,19 @@ public class ServerThread extends Thread {
         Pattern special = Pattern.compile ("[!@#$%&*()+=|<>?{};:,<.>/\\[\\]~-]");
         Matcher hasSpecial = special.matcher(msgArr[1]);
         if(msgArr[1].length()>14 || msgArr[1].length()<3 || hasSpecial.find()){
+            // If the format is invalid, send failure message
             writer.println(Protocol.FAIL02);
             return;
-            //Continue; //continue the outer (while) loop
         }
 
-        //Set username for the client
+        // Set the username for the client
         username = msgArr[1];
         writer.println(Protocol.OK + " " + Protocol.IDENTIFY + " " +  username);
+        // Broadcast message to all clients that the user has joined the chat
         broadcastMessage("JOINED " + username);
     }
 
-    private static class pingPong implements Runnable{
+    private class pingPong implements Runnable{
         ServerThread parent;
 
         pingPong(ServerThread parent){
@@ -200,14 +242,23 @@ public class ServerThread extends Thread {
             while (true) {
                 try {
                     parent.writer.println(Protocol.PING);
-                    // Sleep for 3 seconds
-                    Thread.sleep(3000);
+                    writer.flush();
+                    // Sleep for 5 seconds
+                    Thread.sleep(5000);
 
                     if(!pingPongFlag){
-                        parent.writer.println(Protocol.DISCONNECT + " Pong timeout");
                         parent.writer.println(Protocol.FAIL05);
+                        parent.writer.println(Protocol.DISCONNECT + " Pong timeout");
+
+
+                        //Broadcast that the user has left/been disconnected from the server
+                        parent.broadcastMessage(Protocol.LEFT + " " + parent.username);
+                        writer.flush();
+
                         parent.server.users.remove(this);
-                        socket.close();
+                        //socket.close();
+                        parent.writer.close();
+                        parent.reader.close();
                         parent.stop();
                     }else{
                         //Pong received, reset flag
@@ -223,3 +274,5 @@ public class ServerThread extends Thread {
         }
     }
 }
+
+
